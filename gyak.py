3c4c214b25306406fa0ce8f392005a3ede87cef9
vogronicspatrik <vogronicspatrik@gmail.com>
2016-09-16 16:37:47 +0200

    forever moevement

--- gyak.py
+++ gyak.py
@@ -1,36 +1,75 @@
 import pygame
+from pygame.locals import *
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
-done = False
 is_blue = True
+green = (0, 255, 0)
+red = (255, 0, 0)
 x = 150
 y = 150
+x_change = 0
+y_change = 0
+z = 300
+q = 300
+z_change = 0
+q_change = 0
 
 clock = pygame.time.Clock()
 
+
+done = False
 while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         done = True
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                        is_blue = not is_blue
 
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]:
-            y -= 1
-        if pressed[pygame.K_DOWN]:
-            y += 1
-        if pressed[pygame.K_LEFT]:
-            x -= 1
-        if pressed[pygame.K_RIGHT]:
-            x += 1
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        x_change = -10
+                        y_change = 0
+                    elif event.key == pygame.K_RIGHT:
+                        x_change = 10
+                        y_change = 0
+                    elif event.key == pygame.K_UP:
+                        y_change = -10
+                        x_change = 0
+                    elif event.key == pygame.K_DOWN:
+                        y_change = 10
+                        x_change = 0
+                    if event.key == pygame.K_a:
+                        z_change = -10
+                        q_change = 0
+                    elif event.key == pygame.K_d:
+                        z_change = 10
+                        q_change = 0
+                    elif event.key == pygame.K_w:
+                        z_change = -10
+                        q_change = 0
+                    elif event.key == pygame.K_s:
+                        z_change = 10
+                        q_change = 0
+        # pressed = pygame.key.get_pressed()
+        # if pressed[pygame.K_UP]:
+        #     y -= 1
+        # if pressed[pygame.K_DOWN]:
+        #     y += 1
+        # if pressed[pygame.K_LEFT]:
+        #     x_change -= 10
+        # if pressed[pygame.K_RIGHT]:
+        #     x_change += 10
+        # if pressed[pygame.K_w]:
+        #     q -= 1
+        # if pressed[pygame.K_s]:
+        #     q += 1
+        # if pressed[pygame.K_a]:
+        #     z -= 1
+        # if pressed[pygame.K_d]:
+        #     z += 1
+        x += x_change
+        y += y_change
+        pygame.draw.rect(screen, green, pygame.Rect(x, y, 10, 10))
+        pygame.draw.rect(screen, red, pygame.Rect(z, q, 10, 10))
 
-        if is_blue:
-            color = (0, 128, 255)
-        else:
-            color = (255, 100, 0)
-        pygame.draw.rect(screen, color, pygame.Rect(x, y, 30, 30))
-
-        pygame.display.flip()
-        clock.tick(60)
+        pygame.display.update()
+        clock.tick(20)
